84c60d58e166aec6beea09faf0994e10705cc04c
wh7rb7 <44367179+s1l3nt78@users.noreply.github.com>
2020-10-02 12:30:30 +0200

    Update install-ex.sh

--- install-ex.sh
+++ install-ex.sh
@@ -2,7 +2,7 @@
 mv eDEX-UI -t /home/$USER/.config/
 cp gSifter sifter -t /usr/sbin
 mv gSifter sifter -t /opt/sifter
-sudo chmod +x /usr/sbin/gSifter /usr/sbin/sifter
+sudo chmod +x /usr/sbin/gSifter /usr/sbin/sifter /opt/sifter/gSifter_eDEX-UI.AppImage
 mv gSifter_eDEX-UI.AppImage -t /opt/sifter 
 pwd & cd ..
 sudo rm -rf gSifter
