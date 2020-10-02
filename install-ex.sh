4c4647a1d4be63d430f7a3a34d8c9e69c760ea9a
wh7rb7 <44367179+s1l3nt78@users.noreply.github.com>
2020-10-02 11:39:46 +0200

    Update install-ex.sh

--- install-ex.sh
+++ install-ex.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+####################
+#    Root Check    #
+####################
 
-
-mv eDEX-UI -t /$USER/.config/
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as ROOT USER"
+   exit 1
+fi
+if [[ -d "/root/.config" ]]; then
+  mkdir /root/.config
+ fi
+mv eDEX-UI -t /root/.config/
 cp gSifter sifter -t /usr/sbin
 mv gSifter sifter -t /opt/sifter
 sudo chmod +x /usr/sbin/gSifter /usr/sbin/sifter
