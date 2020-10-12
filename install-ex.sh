bbcfa85436d4be5a3ad17406317a5bc730195ebd
wh7rb7 <44367179+s1l3nt78@users.noreply.github.com>
2020-10-13 01:31:28 +0200

    Update install-ex.sh

--- install-ex.sh
+++ install-ex.sh
@@ -3,10 +3,10 @@ sudo apt-get install nodejs npm libfuse3-3 libfuse3-dev libfuse2 libfuse-dev fus
 mv eDEX-UI -t /home/$USER/.config/
 cp gSifter sifter -t /usr/sbin
 mv gSifter sifter -t /opt/sifter
-sudo chmod +x /usr/sbin/gSifter /usr/sbin/sifter /opt/sifter/gSifter_eDEX-UI.AppImage
+sudo chmod +x /usr/sbin/gSifter /opt/sifter/gSifter_eDEX-UI.AppImage
 mv gSifter_eDEX-UI.AppImage -t /opt/sifter 
-CG=$(pwd | grep gSifter)
-if [[ ${CG} == */"gSifter" ]];then
+CG=$(pwd | grep g)
+if [[ ${CG} == */"g" ]];then
   cd ..
-  sudo rm -rf gSifter
+  sudo rm -rf g
 fi
